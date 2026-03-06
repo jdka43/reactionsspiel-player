0c715dc94eae75d75706dddd268de9acfac35b9f
jdka43 <lukas.gilles@magenta.de>
2026-03-06 15:17:44 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,32 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         basic.setLedColor(0x00ffff)
+        if (input.buttonIsPressed(Button.A)) {
+            radio.sendValue(player, 1)
+        } else if (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.AB)) {
+            radio.sendValue(player, 2)
+        }
     } else if (receivedNumber == 2) {
-    	
+        basic.setLedColor(0xff0000)
+        if (input.buttonIsPressed(Button.B)) {
+            radio.sendValue(player, 2)
+        } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.AB)) {
+            radio.sendValue(player, 1)
+        }
     } else if (receivedNumber == 3) {
-    	
+        if (input.buttonIsPressed(Button.AB)) {
+            basic.setLedColor(0xff0000)
+            radio.sendValue(player, 3)
+        } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            radio.sendValue(player, 1)
+        }
     }
 })
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    radio.sendString("player")
+})
+radio.onReceivedString(function (receivedString) {
+    player = receivedString
+})
+let player = ""
 radio.setGroup(88)
