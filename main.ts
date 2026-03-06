61c5f0e233da7a564d0bc3a6621518d313822bde
jdka43 <lukas.gilles@magenta.de>
2026-03-06 14:17:31 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.setLedColor(0x00ffff)
+    } else if (receivedNumber == 2) {
+    	
+    } else if (receivedNumber == 3) {
+    	
+    }
+})
+radio.setGroup(88)
