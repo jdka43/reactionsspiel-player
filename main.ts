ca86d1d4aa43f8e43164044c3cebf6003b1ec328
jdka43 <lukas.gilles@magenta.de>
2026-03-13 14:32:38 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        basic.setLedColor(0x007fff)
-        if (input.buttonIsPressed(Button.A)) {
-            radio.sendValue(player, 1)
-        } else if (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.AB)) {
-            radio.sendValue(player, 2)
-        }
-    } else if (receivedNumber == 2) {
-        basic.setLedColor(0xff0000)
-        if (input.buttonIsPressed(Button.B)) {
-            radio.sendValue(player, 2)
-        } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.AB)) {
-            radio.sendValue(player, 1)
-        }
-    } else if (receivedNumber == 3) {
-        if (input.buttonIsPressed(Button.AB)) {
-            basic.setLedColor(0xffff00)
-            radio.sendValue(player, 3)
-        } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
-            radio.sendValue(player, 1)
+    if (ist_gedrückt == 1) {
+        if (receivedNumber == 1) {
+            basic.setLedColor(0x007fff)
+            if (input.buttonIsPressed(Button.A)) {
+                radio.sendValue(player, 1)
+            } else if (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.AB)) {
+                radio.sendValue(player, 2)
+            }
+        } else if (receivedNumber == 2) {
+            basic.setLedColor(0xff0000)
+            if (input.buttonIsPressed(Button.B)) {
+                radio.sendValue(player, 2)
+            } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.AB)) {
+                radio.sendValue(player, 1)
+            }
+        } else if (receivedNumber == 3) {
+            if (input.buttonIsPressed(Button.AB)) {
+                basic.setLedColor(0xffff00)
+                radio.sendValue(player, 3)
+            } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+                radio.sendValue(player, 1)
+            }
         }
     }
 })
@@ -26,7 +28,10 @@ radio.onReceivedString(function (receivedString) {
     player = receivedString
 })
 let player = ""
+let ist_gedrückt = 0
+ist_gedrückt = 0
 radio.setGroup(88)
 WaitUntilBlocks.waitUntilButtonPressed(Button.A)
 radio.sendString("player")
-basic.showString("senden")
+basic.showString("send")
+ist_gedrückt = 1
