aaafe1e6739c652032304e171d5a7eedbd22f9f2
jdka43 <lukas.gilles@magenta.de>
2026-03-13 15:09:34 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,22 +4,28 @@ radio.onReceivedNumber(function (receivedNumber) {
             basic.setLedColor(0x007fff)
             if (input.buttonIsPressed(Button.A)) {
                 radio.sendValue(player, 1)
+                basic.setLedColor(0x000000)
             } else if (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.AB)) {
                 radio.sendValue(player, 2)
+                basic.setLedColor(0x000000)
             }
         } else if (receivedNumber == 2) {
             basic.setLedColor(0xff0000)
             if (input.buttonIsPressed(Button.B)) {
                 radio.sendValue(player, 2)
+                basic.setLedColor(0x000000)
             } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.AB)) {
                 radio.sendValue(player, 1)
+                basic.setLedColor(0x000000)
             }
         } else if (receivedNumber == 3) {
             if (input.buttonIsPressed(Button.AB)) {
                 basic.setLedColor(0xffff00)
                 radio.sendValue(player, 3)
+                basic.setLedColor(0x000000)
             } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
                 radio.sendValue(player, 1)
+                basic.setLedColor(0x000000)
             }
         }
     }
@@ -31,6 +37,7 @@ radio.onReceivedString(function (receivedString) {
     basic.showString(player)
     basic.pause(1000)
     basic.clearScreen()
+    ist_gedrückt = 1
 })
 let player = ""
 let ist_gedrückt = 0
@@ -38,8 +45,7 @@ ist_gedrückt = 0
 radio.setGroup(88)
 WaitUntilBlocks.waitUntilButtonPressed(Button.A)
 if (input.buttonIsPressed(Button.A)) {
-    radio.sendString("player")
     radio.setGroup(99)
+    radio.sendString("player")
     basic.showString("send")
-    ist_gedrückt = 1
 }
